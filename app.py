4d735ac70f5a7ca63f05043d8e041b89d9171c44
Gabriel <gabriel0llerena@gmail.com>
2026-04-28 19:11:11 -0500

    Agregar CORS

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
+from flask_cors import CORS
 from flask import Flask, jsonify, request
 from mssql_python import connect
 from email_service import enviar_correo_alerta
 
 app = Flask(__name__)
-
+CORS(app)
 
 def get_connection():
     server = os.getenv("DB_SERVER")
